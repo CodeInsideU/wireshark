3c045c1dd41aaf952ae032edcda9092d6fc15262
Vratik Zade <vratikzade2001@gmail.com>
2023-09-15 09:32:31 +0530

    New_features

--- test.js
+++ test.js
@@ -1,12 +1,26 @@
-const { capturePackets } = require('./wireshark');
+const chai = require('chai');
+const expect = chai.expect;
+const { capturePackets, stopCapture } = require('./wireshark.js'); // Replace with the actual path to your package module
 
-async function main() {
-    try {
+describe('Packet Capture (Original Code)', () => {
+    let captureProcess;
 
-        await capturePackets('Wi-Fi');
-    } catch (error) {
-        console.error(error.message);
-    }
-}
+    beforeEach(() => {
+        // Start the packet capture using the original code before each test
+        captureProcess = capturePackets('Wi-Fi');
+    });
 
-main();
+    afterEach(async () => {
+        // Stop the packet capture after each test
+        await stopCapture();
+    });
+
+    it('should capture packets', async function () {
+        this.timeout(0); // Disable the timeout for this test
+        // The test will run indefinitely until you manually stop the terminal.
+        const result = await captureProcess;
+        expect(result).to.equal('Packet capture completed');
+    });
+
+    // Add more test cases as needed
+});
--- wireshark.js
+++ wireshark.js
@@ -1,22 +1,66 @@
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
+const fs = require('fs');
+
+let childProcess;
 
 async function capturePackets(interface) {
-    return new Promise(async (resolve, reject) => {
-        const command = `tshark -i ${interface} -l -T fields -e frame.time -e eth.src -e eth.dst -e ip.src -e ip.dst -e tcp.srcport -e tcp.dstport -e data`;
-        const child = exec(command);
+    return new Promise((resolve, reject) => {
+        const command = `tshark`;
+        const args = [
+            '-i',
+            interface,
+            '-l',
+            '-T',
+            'fields',
+            '-e',
+            'frame.time',
+            '-e',
+            'eth.src',
+            '-e',
+            'eth.dst',
+            '-e',
+            'ip.src',
+            '-e',
+            'ip.dst',
+            '-e',
+            'tcp.srcport',
+            '-e',
+            'tcp.dstport',
+            '-e',
+            'data',
+        ];
+
+        const child = spawn(command, args);
+        childProcess = child;
+
+        const logFile = fs.createWriteStream('packet_log.txt', { flags: 'a' }); // Append mode
+
+        child.stdout.pipe(logFile);
+        child.stderr.pipe(process.stderr); // Redirect stderr to the console for error output.
 
         child.stdout.on('data', (data) => {
-            // Parse and display Ethernet packet data
-            const [timestamp, ethSrc, ethDst, ipSrc, ipDst, srcPort, dstPort, packetData] = data.split('\t');
-            console.log('Timestamp:', timestamp);
-            console.log('Ethernet Source:', ethSrc);
-            console.log('Ethernet Destination:', ethDst);
-            console.log('IP Source:', ipSrc);
-            console.log('IP Destination:', ipDst);
-            console.log('Source Port:', srcPort);
-            console.log('Destination Port:', dstPort);
-            console.log('Packet Data:', packetData);
-            console.log('\n');
+            const lines = data.toString().split('\n').filter(line => line.trim() !== ''); // Split lines and filter out empty lines
+
+            for (const line of lines) {
+                const [timestamp, ethSrc, ethDst, ipSrc, ipDst, srcPort, dstPort, packetData] = line.split('\t');
+                console.log('Timestamp:', timestamp);
+                console.log('Ethernet Source:', ethSrc);
+                console.log('Ethernet Destination:', ethDst);
+                console.log('IP Source:', ipSrc);
+                console.log('IP Destination:', ipDst);
+                console.log('Source Port:', srcPort);
+                console.log('Destination Port:', dstPort);
+
+                if (packetData) {
+                    console.log('Packet Data:', packetData);
+                }
+
+                console.log('\n');
+
+                if (packetData) {
+                    logFile.write(`${timestamp}\t${ethSrc}\t${ethDst}\t${ipSrc}\t${ipDst}\t${srcPort}\t${dstPort}\t${packetData}\n`);
+                }
+            }
         });
 
         child.on('close', (code) => {
@@ -33,6 +77,19 @@ async function capturePackets(interface) {
     });
 }
 
+async function stopCapture() {
+    if (childProcess) {
+        childProcess.kill();
+        childProcess = null; // Reset the child process variable
+    }
+}
+
+process.on('SIGINT', () => {
+    stopCapture();
+    process.exit();
+});
+
 module.exports = {
     capturePackets,
+    stopCapture,
 };
